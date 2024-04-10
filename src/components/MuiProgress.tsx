0d98037e1a95e9dd61f3fcfd09ca36e39db02ff8
Abhishek <Abhishek@apples-MacBook-Air-2.local>
2024-04-10 08:03:15 +0530

    complete React MUI practice

---
+++ src/components/MuiProgress.tsx
@@ -0,0 +1,13 @@
+import { Stack, CircularProgress, LinearProgress } from "@mui/material";
+export const MuiProgress = () => {
+  return (
+    <Stack spacing={2}>
+      <CircularProgress />
+      <CircularProgress color="warning" />
+      <CircularProgress variant="determinate" value={60} />
+      <LinearProgress />
+      <LinearProgress color="error" />
+      <LinearProgress variant="determinate" value={60} />
+    </Stack>
+  );
+};
